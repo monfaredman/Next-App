92868b1b3795a0fa194e38fb58a673dcc2b4051c
monfaredman <monfaredman@outlook.com>
2023-07-24 13:50:34 +0330

    first-commit

---
+++ src/layout.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import MainHeader from "./components/layout/main-header";
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+
+export default Layout;
