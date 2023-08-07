cc65db86fdb10c81799d6b9363551d31ce1e66f0
monfaredman <monfaredman@outlook.com>
2023-08-07 17:54:14 +0330

    add-profile-auth

--- src/components/layout/main-header.js
+++ src/components/layout/main-header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import Button from "@/components/ui/button";
 import classes from "./main-header.module.css";
 
 function MainHeader() {
@@ -12,6 +12,15 @@ function MainHeader() {
         <ul>
           <li>
             <Link href="/events">Browse All Events</Link>
+          </li>
+          <li>
+            <Link href="/auth">Login</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li>
+            <Button>Logout</Button>
           </li>
         </ul>
       </nav>
